24b0d4615add1e64e1bc354db9addc4114c0b3a8
DairaGarraza <daira.garraza@gmail.com>
2019-08-21 15:56:17 -0300

    primer commit

---
+++ clase1_(20-08)/subasta/src/ar/edu/unlam/pb2/subasta/Subasta.java
@@ -0,0 +1,27 @@
+package ar.edu.unlam.pb2.subasta;
+
+public class Subasta {
+
+	private Integer valor;
+	private boolean abierta;
+
+	public Subasta(Integer valor) {
+		this.valor = valor;
+		this.abierta = true;
+	}
+
+	public void ofertar(Integer valor) {
+
+		if (abierta == true && this.valor < valor) {
+			this.valor = valor;
+		}
+	}
+
+	public Integer cerrar() {
+
+		this.abierta = false;
+		return this.valor;
+
+	}
+
+}
---
+++ clase1_(20-08)/subasta/test/ar/edu/unlam/pb2/subasta/SubastaTest.java
@@ -0,0 +1,31 @@
+package ar.edu.unlam.pb2.subasta;
+
+import org.junit.Assert; 
+import org.junit.Test;
+
+public class SubastaTest {
+
+	@Test
+	public void testQueSePuedaCrearUnaSubasta() {
+		Subasta miSubasta = new Subasta(40);
+	}
+
+	@Test
+	public void testQueUnaVezCerradaNoRecibaMasOfertas() {
+		
+		// 1.Preparacion
+		Integer valorInicial = 100;
+		Subasta miSubasta = new Subasta(valorInicial);
+		Integer primerOferta = 150;
+		Integer ultimaOferta = 250;
+
+		// 2.Ejecución
+		miSubasta.ofertar(primerOferta);
+		Integer valorEsperado = miSubasta.cerrar();
+		miSubasta.ofertar(ultimaOferta);
+		Integer valorObtenido = miSubasta.cerrar();
+
+		// 3.Contrastación
+		Assert.assertEquals(valorEsperado, valorObtenido);
+	}
+}

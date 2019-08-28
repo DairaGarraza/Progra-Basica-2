ee810f1b0d60ce10ab90e77fefbc979b8128be18
DairaGarraza <49700071+DairaGarraza@users.noreply.github.com>
2019-08-27 22:05:25 -0300

    actualización clase 2

--- TesteFecha.java
+++ TesteFecha.java
@@ -1,5 +1,22 @@
 package ar.edu.unlam.pb2.clase2;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TesteFecha {
 
+	@Test
+	public void testQueVerifiqueQDosFechassSonIguales() {
+		//1.Preparación
+		
+		Fecha fecha1 = new Fecha(27, 8, 2019);
+		
+		Fecha fecha2 = new Fecha(27, 8, 2019);
+		
+		//2.Ejecución
+		
+		
+		//3.Contrastación
+		Assert.assertEquals(fecha1, fecha2);
+	}
 }
